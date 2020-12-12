49398d7d75ae64085608496b648b1da66682442a
Stephen Sepan <sjsepan@outlook.com>
2020-12-12 14:38:17 -0500

    chgs for FW 4.8 and SDK projects; cannot ref regex nuget pkg?

--- ExpressionTreeTestConsole/LambdaCalc.cs
+++ ExpressionTreeTestConsole/LambdaCalc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
+//using System.Text;
 using System.Text.RegularExpressions;
 
 namespace LambdaCalc
